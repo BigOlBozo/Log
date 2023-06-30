7a6995b3ce920e87fba1816fcc9208ffeeb67ab2
BigBozo <merpdameep1@gmail.com>
2023-06-30 10:01:17 -0500

    done

--- Logger.py
+++ Logger.py
@@ -1,31 +1,16 @@
-from time import perf_counter as click
 from datetime import datetime as dt
-from time import sleep
-pp = []
-cs = []
-def check_input():
-    if input() == 'p':
-        pause_start = dt.today()
-        with open('Log.txt', 'a') as f:
-            f.write(f'Pause Start: {pause_start}\n')
-    if input() == 'o':
-        pause_end = dt.today()
-        with open('Log.txt', 'a') as f:
-            f.write(f'Pause End: {pause_end}\n')
-    if input() == 'a':
-        pass
-    
+
 def chk_input(st):
-    q = input('S/E?')
-    if q == 'o':
+    q = input('S/E? ')
+    if q == 's':
         with open('Log.txt', 'a') as f:
             f.write(f'Start: {dt.today()}\n')
         start = dt.today()
         print(f'Start: {start}')
         chk_input(start)
 
-    if q == 'p':
-        print(f'End: {dt.today()}')
+    if q == 'e':
+        print(f'End: {dt.today()}\nElapsed: {dt.today()-st}')
         with open('Log.txt', 'a') as f:
             f.write(f'End: {dt.today()}\n')
             f.write(f'Elapsed: {dt.today()-st}\n\n')
